7e9829a2025c7a131f71c36f108423faca0ea5ce
reannu123 <reannumon123@gmail.com>
2023-04-13 19:21:07 +0800

    Fixed sizing of sections and scrolling

--- reannu123.github.io/src/App.tsx
+++ reannu123.github.io/src/App.tsx
@@ -11,24 +11,39 @@ function App() {
   const [isDark, setIsDark] = useState(false);
   const [currentScreen, setCurrentScreen] = useState(<Summary />);
   return (
-    <div className="sh-screen w-screen">
+    <div className="sh-screen w-full ">
       {/* Navbar */}
       <NavBar setCurrentScreen={setCurrentScreen} />
       {/* Main */}
       <div className="flex flex-col items-center justify-center">
-        <section id="Summary" className="h-auto bg-cyan-600">
+        <section
+          id="Summary"
+          className="scroll-pt-36 w-full h-auto bg-cyan-600 "
+        >
           <Summary />
         </section>
-        <section id="Skills" className="w-screen h-screen bg-cyan-500">
+        <section
+          id="Skills"
+          className="scroll-pt-36 w-full h-screen text-center bg-cyan-500"
+        >
           <Skills />
         </section>
-        <section id="Education" className="w-screen h-screen bg-cyan-400">
+        <section
+          id="Education"
+          className="scroll-pt-36 w-full h-screen text-center bg-cyan-400"
+        >
           <Education />
         </section>
-        <section id="Projects" className="w-screen h-screen bg-cyan-300">
+        <section
+          id="Projects"
+          className="scroll-pt-36 w-full h-screen text-center bg-cyan-300"
+        >
           <Projects />
         </section>
-        <section id="Others" className="w-screen h-screen bg-cyan-300">
+        <section
+          id="Others"
+          className="scroll-pt-36 w-full h-screen text-center bg-cyan-300"
+        >
           <Hobbies />
         </section>
       </div>
--- reannu123.github.io/src/components/NavBar.tsx
+++ reannu123.github.io/src/components/NavBar.tsx
@@ -9,45 +9,55 @@ import Projects from "./Projects";
 function NavBar(props: any) {
   return (
     <header className="sticky bg-cyan-100  top-0  flex ">
-      <nav className=" w-screen flex items-center justify-between p-4 ">
+      <nav className=" max-md:flex-col flex-wrap w-full  flex items-center justify-between p-4 ">
         <div className=" flex flex-row space-x-8 items-center justify-center mx-8 whitespace-nowrap">
           <a href="https://github.com/reannu123">
             <img
               src="https://avatars.githubusercontent.com/u/54865395?s=400&v=4"
-              className="h-8 w-8 rounded-full "
+              className="h-8 w-8 rounded-full"
             />
           </a>
-          <span className="self-center font-bold text-xl md:text-2xl lg:text-4xl font-mono w-auto sm:text-center">
+          <span className="max-md:text-center font-bold text-2xl font-mono w-auto ">
             Reannu Instrella
           </span>
         </div>
 
-        <div className="hidden w-full md:block md:w-auto " id="navbar-default">
-          <ul className="font-medium flex flex-row max-md:hidden space-x-8 items-center justify-between mx-8 mt-0">
+        <div className=" w-full md:block md:w-auto " id="navbar-default">
+          <ul className="font-medium flex flex-wrap  space-x-8 items-center justify-between mx-8 mt-0">
             <li>
-              <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white  hover:cursor-pointer transition-all">
-                <a href="#Summary">Summary</a>
-              </div>
+              <a href="#Summary">
+                <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white  hover:cursor-pointer transition-all">
+                  Summary
+                </div>
+              </a>
             </li>
             <li>
-              <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
-                <a href="#Skills">Skills</a>
-              </div>
+              <a href="#Skills">
+                <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
+                  Skills
+                </div>
+              </a>
             </li>
             <li>
-              <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
-                <a href="#Education">Education</a>
-              </div>
+              <a href="#Education">
+                <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
+                  Education
+                </div>
+              </a>
             </li>
             <li>
-              <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
-                <a href="#Projects">Projects</a>
-              </div>
+              <a href="#Projects">
+                <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
+                  Projects
+                </div>
+              </a>
             </li>
             <li>
-              <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
-                <a href="#Others">Others</a>
-              </div>
+              <a href="#Others">
+                <div className="block py-2 pl-3 pr-4 rounded hover:bg-cyan-900 hover:text-white hover:cursor-pointer transition-all">
+                  Others
+                </div>
+              </a>
             </li>
           </ul>
         </div>
