49be01ac876e4602864dcfcd322b964e344fb9b1
reannu123 <reannumon123@gmail.com>
2023-06-08 15:50:18 +0800

    Added resume download

--- reannu123.github.io/src/components/NavBar.tsx
+++ reannu123.github.io/src/components/NavBar.tsx
@@ -29,8 +29,18 @@ function NavBar(props: any) {
         <div className=" hidden w-full md:block md:w-auto " id="navbar-default">
           <ul className="font-medium flex flex-wrap  items-center justify-between mx-8 md:space-x-3 text-sm lg:text-xl lg:space-x-3">
             <li>
-              <a href="#Summary">
+              <a
+                href="src/assets/Reannu_Instrella_Resume.pdf"
+                download={"Reannu_Instrella_Resume"}
+              >
                 <div className="block py-2  px-3 rounded-lg  bg-gradient-to-l from-teal-300 via-teal-500 to-teal-600  bg-size-200 hover:bg-right hover:text-gray-900  hover:cursor-pointer  dark:text-gray-100 dark:hover:text-gray-900 transition-all ">
+                  Resume
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="#Summary">
+                <div className="block py-2  px-3  rounded-lg hover:bg-teal-500  hover:text-gray-700 hover:cursor-pointer dark:text-gray-100 transition dark:hover:text-gray-900 ">
                   Summary
                 </div>
               </a>
